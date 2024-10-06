88c33c7b2fddfa65248647982c31c83078d23cd1
Keysya Alifia Zabina <163361900+keysyaalifiazabina@users.noreply.github.com>
2024-10-06 15:44:06 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -211,45 +211,48 @@ st.pyplot(plt)
 
 # Best Customer Based on RFM Parameters
 st.subheader("Best Customer Based on RFM Parameters")
- 
+
 col1, col2, col3 = st.columns(3)
- 
+
 with col1:
     avg_recency = round(rfm_df.recency.mean(), 1)
     st.metric("Average Recency (days)", value=avg_recency)
- 
+
 with col2:
     avg_frequency = round(rfm_df.frequency.mean(), 2)
     st.metric("Average Frequency", value=avg_frequency)
- 
-with col3:
-    avg_monetary = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
-    st.metric("Average Monetary", value=avg_monetary)
- 
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
-colors = ["#624E88"] * 5  # Menggunakan warna yang sama untuk semua bar
 
+with col3:
+    avg_monetary = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO')
+    st.metric("Average Monetary", value=avg_monetary)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#624E88"] * 5  # Use the same color for all bars
+
+# Plotting Recency
 sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("customer_id") 
+ax[0].set_ylabel("Recency (days)", fontsize=18)
+ax[0].set_xlabel("Customer ID", fontsize=18)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
 ax[0].tick_params(axis='x', labelsize=15)
-ax[0].set_xticklabels([])  
+ax[0].set_xticklabels([])  # Remove x-tick labels if needed
 
+# Plotting Frequency
 sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("customer_id") 
+ax[1].set_ylabel("Frequency", fontsize=18)
+ax[1].set_xlabel("Customer ID", fontsize=18)
 ax[1].set_title("By Frequency", loc="center", fontsize=18)
 ax[1].tick_params(axis='x', labelsize=15)
-ax[1].set_xticklabels([])  
+ax[1].set_xticklabels([])
 
+# Plotting Monetary
 sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
-ax[2].set_ylabel(None)
-ax[2].set_xlabel("customer_id")  
+ax[2].set_ylabel("Monetary Value", fontsize=18)
+ax[2].set_xlabel("Customer ID", fontsize=18)
 ax[2].set_title("By Monetary", loc="center", fontsize=18)
 ax[2].tick_params(axis='x', labelsize=15)
 ax[2].set_xticklabels([])
- 
+
 st.pyplot(fig)
 
 st.caption('Copyright (c) Keysya Alifia Zabina')
