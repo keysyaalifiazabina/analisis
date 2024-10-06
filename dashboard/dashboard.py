e9f1d7f9125c21bea88f4a27d03e2dfdd60aef01
keysyaalifiazabina <keysyaalifiazabina@gmail.com>
2024-10-06 19:27:24 +0700

    edited

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -52,18 +52,14 @@ def create_most_seller_df(df):
 
 def create_rfm_df(df):
 
-    rfm_df = all_df.groupby(by="customer_id", as_index=False).agg({
-    "order_approved_at": "max",
-    "order_id": "nunique",
-    "payment_value": "sum"
+    rfm_df = all_df.groupby("customer_id", as_index=False).agg({
+        "order_approved_at": "max",  
+        "order_id": "nunique",
+        "payment_value": "sum"
     })
     rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
-
-    # RFM day
-    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
-    recent_date = all_df["order_approved_at"].dt.date.max()
-    rfm_df = rfm_df.dropna(subset=["max_order_timestamp"])
-    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+    recent_date = df["order_approved_at"].max()
+    rfm_df["recency"] = (recent_date - rfm_df["max_order_timestamp"]).dt.days
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
     return rfm_df
 
@@ -81,7 +77,8 @@ for column in datetime_columns:
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(drop=True, inplace=True)
 
-min_date, max_date = all_df["order_approved_at"].min(), all_df["order_approved_at"].max()
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
 
 # Sidebar Streamlit
 with st.sidebar:
