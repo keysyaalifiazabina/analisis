fe7d34f576c88be496934d4567ff90d8e8e654f2
keysyaalifiazabina <keysyaalifiazabina@gmail.com>
2024-10-06 17:38:39 +0700

    edited

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -50,7 +50,7 @@ def create_most_seller_df(df):
     return most_seller_df
 
 def create_rfm_df(df):
-    orders_df = pd.read_csv("dashboard/all_df.csv")
+    orders_df = pd.read_csv("Data/orders_dataset.csv")
     # Mengonversi kolom tanggal
     orders_df['order_approved_at'] = pd.to_datetime(orders_df['order_approved_at'], errors='coerce')
     orders_df = orders_df.dropna(subset=['order_approved_at'])
